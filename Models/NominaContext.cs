9d2aeac438f846361d1f5175228810b520662e3d
191G0235@rcarbonifera.tecnm.mx <191G0235@rcarbonifera.tecnm.mx>
2022-05-09 16:53:28 -0500

    Codificacion de Agregar, Eliminar y guardar command

--- Models/NominaContext.cs
+++ Models/NominaContext.cs
@@ -25,7 +25,7 @@ namespace ControlEmpleados.Models
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseMySql("server=localhost;database=Nomina;user=root;password=Chivas12", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.18-mysql"));
+                optionsBuilder.UseMySql("server=localhost;database=Nomina;user=root;password=root", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.18-mysql"));
             }
         }
 
--- ViewModels/EmpleadosViewModel.cs
+++ ViewModels/EmpleadosViewModel.cs
@@ -30,17 +30,17 @@ namespace ControlEmpleados.ViewModels
         public Empleado Empleado
         {
             get { return empleado; }
-            set { empleado = value; Actualizar("Empleado");}
+            set { empleado = value; Notificar("Empleado");}
         }
 
         //Propiedad para manejar los erroes
 
         private string error;
 
-        public string Erorr
+        public string Error
         {
             get { return error; }
-            set { error = value; Actualizar("Error"); }
+            set { error = value; Notificar("Error"); }
         }
 
         //Propiedad para manejar lo que hay dentro del usercontrol
@@ -56,30 +56,71 @@ namespace ControlEmpleados.ViewModels
         public ObservableCollection<Empleado> Empleados
         {
             get { return empleados; }
-            set { empleados = value; Actualizar("Empleados"); }
+            set { empleados = value; Notificar("Empleados"); }
         }
 
 
-
+        //Constructor
         public EmpleadosViewModel()
         {
             AgregarCommand = new RelayCommand(Agregar);
+            EliminarCommand = new RelayCommand(Eliminar);
+            EditarCommand = new RelayCommand(Editar);
+            CancelarCommand = new RelayCommand(Cancelar);
+            GuardarCommand = new RelayCommand(Guardar);
+
             Categorias = new(context.Categoria);
             ActualizarListaEmpleados();
+            Notificar("");
+        }
+
+        private void Eliminar()
+        {
+            Error = "";
+            context.Empleados.Remove(context.Empleados.FirstOrDefault(x => x.Id == Empleado.Id));
+            context.SaveChanges();
+        }
+
+        private void Guardar()
+        {
+            if(Empleado != null)
+            {
+                Error = "";
+                if (string.IsNullOrWhiteSpace(Empleado.Nombre))
+                    Error += "Escriba el nombre del emplado" + Environment.NewLine;
+                if (Empleado.Sueldo <= 0)
+                    Error += "El sueldo del empleado debe ser mayor a $0.00" + Environment.NewLine;
+
+                Notificar("Error");
+
+                if(Error == "")
+                {
+                    if (Empleado.Id == 0)
+                        context.Add(Empleado);
+                    else
+                        context.Update(Empleado);
+
+                    context.SaveChanges();
+                    Notificar("");
+                }
+            }
         }
 
         private void Agregar()
         {
-            throw new NotImplementedException();
+            Empleado = new();
+            Error = "";
+            Modo = "Agregar";
+            Notificar("");
         }
 
         //Aqui actualizamos empleados
         private void ActualizarListaEmpleados()
         {
             Empleados = new(context.Empleados.Include(x=> x.IdCategoriaNavigation).OrderBy(x=>x.Nombre));
-            Actualizar("Empleados");
+            Notificar("Empleados");
         }
-        public void Actualizar(string nombre)
+        public void Notificar(string nombre)
         {
             PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nombre));
         }
